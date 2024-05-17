2c10eb71e8e587ac028e4acab060a469e4fa93f4
CT Nava <ctnava97@gmail.com>
2024-05-17 11:15:43 -0700

    legibility update

--- bin/clone.ps1
+++ bin/clone.ps1
@@ -1,11 +1,15 @@
 Write-Host "`nDownloading Remote Repositories..." -ForegroundColor Green
+
 Write-Host "`n||Fetching Backend..." -ForegroundColor Yellow
 git clone https://github.com/NIOV-Labs/mongodb-api.git niov-backend
 Write-Host "||Done!" -ForegroundColor Green
+
 Write-Host "`n||Fetching Contracts..." -ForegroundColor Yellow
 git clone https://github.com/NIOV-Labs/protocol-v1.git niov-contracts
 Write-Host "||Done!" -ForegroundColor Green
+
 Write-Host "`n||Fetching Frontend..." -ForegroundColor Yellow
 git clone https://github.com/NIOV-Labs/frontend.git niov-frontend
 Write-Host "||Done!" -ForegroundColor Green
+
 Write-Host "Fetched Remote Repositories" -ForegroundColor Green
