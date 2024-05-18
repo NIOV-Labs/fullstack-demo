07278f66fccf3ab8790d3e894b55acce7fb48be9
CT Nava <ctnava97@gmail.com>
2024-05-18 15:37:56 -0700

    install script fix

--- bin/install.ps1
+++ bin/install.ps1
@@ -2,6 +2,10 @@ $projectRoot = Get-Location
 $folders = @('niov-backend', 'niov-contracts', 'niov-frontend')
 Write-Host "`nInstalling Dependencies..." -ForegroundColor Green
 
+Write-Host "`n||Installing root dependencies..." -ForegroundColor Yellow
+npm i
+Write-Host "||Dependencies Installed" -ForegroundColor Green
+
 foreach ($folder in $folders) {
     $folderPath = "$projectRoot/$folder"
     Set-Location $folderPath
